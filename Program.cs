6a2aedf046aa5fcdb30b5a7a71327026903cd43c
Morti <morti.martinez-seara.7e9@itb.cat>
2025-11-27 10:28:05 +0100

    feat: add exercice one

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main()
+    {
+        int MinValue = 10;
+        int MaxValue = 50;
+        int num1 = 6;
+        int num2 = 22;
+
+        Console.WriteLine(IsInRange(num1, MinValue, MaxValue) ? "In range" : "Not in range");
+        Console.WriteLine(IsInRange(num2, MinValue, MaxValue) ? "In range" : "Not in range");
+    }
+    public static bool IsInRange(int num, int minValue, int maxValue){
+        return (num <= maxValue && num >= minValue);
+    }
+}
