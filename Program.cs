577a50667271b97d59bb11c92fce35f1d6ee8473
mortimtzseara <morti.martinez-seara.7e9@itb.cat>
2025-12-02 18:44:23 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
-﻿using System;
+using System;
+using System.Text.RegularExpressions;
 
 public class Program
 {
     public static void Main()
     {
+        const string MsgInput = "Give me your DNI/NIE:";
+        const string MsgOk = "The document is valid";
+        const string MsgKo = "The document is not valid";
 
+        string documentNumber;
+
+        Console.WriteLine(MsgInput);
+        documentNumber = Console.ReadLine() ?? "";
+
+        if (IsValidDocument(documentNumber))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(MsgOk);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(MsgKo);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public static bool IsValidDocument(string documentNumber)
+    {
+        const string RegexNIE = @"^[XYZ]\d{7}[A-Z]$";
+        const string RegexDNI = @"^\d{8}[A-Z]$";
+
+        if (Regex.IsMatch(documentNumber, RegexDNI) || Regex.IsMatch(documentNumber, RegexNIE))
+            return true;
+
+        return false;    
     }
 }
